3f3228e4a8925cd39e8a7bc18d51123d3b541c8d
Wilhelm Werwein <werwein.dev@gmail.com>
2023-07-31 12:09:01 +0200

    comment for future reference

--- js/index.js
+++ js/index.js
@@ -6,12 +6,6 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   const newFormData = new FormData(e.target);
   const data = Object.fromEntries(newFormData);
-
-  if (data.password != data.passwordRepeat) {
-    passwordInput1.style.backgroundColor = "#cb3737a6";
-    passwordInput2.style.backgroundColor = "#cb3737a6";
-    passwordInput1.style.border = "2px solid #cb3737";
-    passwordInput2.style.border = "2px solid #cb3737";
-    const errorMessagePassword = document.createElement("p");
-  }
 });
+
+// ! Make Password Border red if they don't match
